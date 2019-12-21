9a562cca189965ba4ff5619532ef95236854fad3
S <Palanisamy.X.S@questdiagnostics.com>
2019-12-21 15:20:33 -0500

    hibernate webservice changes

---
+++ src/test/java/com/javaspringclub/service/test/ParentServiceImplTest.java
@@ -0,0 +1,5 @@
+package com.javaspringclub.service.test;
+
+public class ParentServiceImplTest {
+
+}
---
+++ src/test/java/com/javaspringclub/service/test/TestClient.java
@@ -0,0 +1,12 @@
+package com.javaspringclub.service.test;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("TestClient is Called.");
+
+	}
+
+}
